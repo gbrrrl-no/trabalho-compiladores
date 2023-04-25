154c3d758409dbfd80df2257220f312ff9ec4dcc
Gabriel Nóbrega <gabrielnobrega63@gmail.com>
2023-04-25 14:03:10 -0300

    feita a parte 3

--- PARTE_3/RevPN.py
+++ PARTE_3/RevPN.py
@@ -1,3 +1,4 @@
+import re
 from enum import Enum
 
 
@@ -18,21 +19,32 @@ class Token:
         return f"Token [type={self.type}, lexeme={self.lexeme}]"
 
 
-def scan(source):
+class Regex:
+    @staticmethod
+    def scan(source):
+        pattern = re.compile(r'(\d+|[+\-*/])')
+        return pattern.findall(source)
+
+
+def scan_using_regex(source):
+    regex = Regex()
+    return regex.scan(source)
+
+
+def tokenize(rpn_expression):
     tokens = []
-    for char in source:
-        if char.isdigit():
-            tokens.append(Token(TokenType.NUM, char))
-        elif char == '+':
-            tokens.append(Token(TokenType.PLUS, char))
-        elif char == '-':
-            tokens.append(Token(TokenType.MINUS, char))
-        elif char == '*':
-            tokens.append(Token(TokenType.MULTIPLY, char))
-        elif char == '/':
-            tokens.append(Token(TokenType.DIVIDE, char))
+    for match in rpn_expression:
+        if match.isdigit():
+            tokens.append(Token(TokenType.NUM, match))
         else:
-            raise ValueError(f"Unexpected character: {char}")
+            if match == '+':
+                tokens.append(Token(TokenType.PLUS, match))
+            elif match == '-':
+                tokens.append(Token(TokenType.MINUS, match))
+            elif match == '*':
+                tokens.append(Token(TokenType.MULTIPLY, match))
+            elif match == '/':
+                tokens.append(Token(TokenType.DIVIDE, match))
 
     return tokens
 
@@ -60,7 +72,7 @@ def rpn_evaluator(token_list):
 
 def read_rpn_expression_from_file(file_path):
     with open(file_path, 'r') as file:
-        rpn_expression = file.read().split()
+        rpn_expression = file.read()
     return rpn_expression
 
 
@@ -69,7 +81,8 @@ if __name__ == '__main__':
 
     rpn_expression = read_rpn_expression_from_file(input_file_path)
     try:
-        token_list = scan(rpn_expression)
+        rpn_expression_scanned = scan_using_regex(rpn_expression)
+        token_list = tokenize(rpn_expression_scanned)
         print(f"Lista de tokens reconhecida: {token_list}")
         result = rpn_evaluator(token_list)
         print(f'Resultado: {result}')
